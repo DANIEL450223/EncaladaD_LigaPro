dbf88fde76cde889c355871e7bf559ec1535bed8
Daniel <emil.encalada@udla.edu.ec>
2025-04-08 08:18:04 -0500

    Test Commits

--- EncaladaD_LigaPro/Controllers/HomeController.cs
+++ EncaladaD_LigaPro/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace EncaladaD_LigaPro.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger) 
         {
             _logger = logger;
         }
